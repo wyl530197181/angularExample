e8fca3e4094b0c5175c71e733fa79ed39649e98b
王园龙 <530197181@qq.com>
2017-10-17 09:59:33 +0800

    产品展示组件样式

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
+  private products: Array<Product>;
   constructor() { }
 
   ngOnInit() {
+    // 组件的生命周期初始化数据
+    this.products = [
+      new Product(1, ' 第一个商品', 1.99, 3.5, '这是第一个商品,我是在慕课网学习的', ['电子产品', '硬件设备']),
+      new Product(2, ' 第二个商品', 0.99, 5.5, '这是第二个商品,我是在慕课网学习的', ['电子产品', '硬件设备']),
+      new Product(3, ' 第三个商品', 1.99, 3.6, '这是第三个商品,我是在慕课网学习的', ['电子产品', '硬件设备']),
+      new Product(4, ' 第四个商品', 2.00, 3.0, '这是第四个商品,我是在慕课网学习的', ['电子产品', '硬件设备']),
+      new Product(5, ' 第五个商品', 3.99, 8.5, '这是第五个商品,我是在慕课网学习的', ['电子产品', '硬件设备']),
+      new Product(5, ' 第六个商品', 10, 8.5, '这是第五个商品,我是在慕课网学习的', ['电子产品', '硬件设备']),      
+    ]
   }
 
+}
+export class Product {
+  constructor(
+    public id: number,
+    public title: string,
+    public price: number,
+    public rating: number,
+    public des: string,
+    public categories: Array<string>
+  ) {
+
+  }
 }
