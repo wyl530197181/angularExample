a1ba04d8f900667cc30b0fd7344f40dd12d8f007
王园龙 <530197181@qq.com>
2017-10-18 21:55:51 +0800

    创建路由  店家跳转商品详情页

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { SearchComponent } from './search/search.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { ProductComponent } from './product/product.component';
-import { StarComponent } from './star/star.component';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {FooterComponent} from './footer/footer.component';
+import {SearchComponent} from './search/search.component';
+import {CarouselComponent} from './carousel/carousel.component';
+import {ProductComponent} from './product/product.component';
+import {StarComponent} from './star/star.component';
+import {ProductDetailComponent} from './product-detail/product-detail.component';
+import {HomeComponent} from './home/home.component';
+import {RouterModule, Routes} from "@angular/router";
 
+const routeConfig: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'product/:productTitle', component: ProductDetailComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,12 +24,16 @@ import { StarComponent } from './star/star.component';
     SearchComponent,
     CarouselComponent,
     ProductComponent,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routeConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
   private products: Array<Product>;
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit() {
     // 组件的生命周期初始化数据
@@ -17,20 +19,19 @@ export class ProductComponent implements OnInit {
       new Product(3, ' 第三个商品', 1.99, 3.6, '这是第三个商品,我是在慕课网学习的', ['电子产品', '硬件设备']),
       new Product(4, ' 第四个商品', 2.00, 3.0, '这是第四个商品,我是在慕课网学习的', ['电子产品', '硬件设备']),
       new Product(5, ' 第五个商品', 3.99, 8.5, '这是第五个商品,我是在慕课网学习的', ['电子产品', '硬件设备']),
-      new Product(5, ' 第六个商品', 10, 8.5, '这是第五个商品,我是在慕课网学习的', ['电子产品', '硬件设备']),      
-    ]
+      new Product(6, ' 第六个商品', 10, 8.5, '这是第五个商品,我是在慕课网学习的', ['电子产品', '硬件设备']),
+    ];
   }
 
 }
+
 export class Product {
-  constructor(
-    public id: number,
-    public title: string,
-    public price: number,
-    public rating: number,
-    public des: string,
-    public categories: Array<string>
-  ) {
+  constructor(public id: number,
+              public title: string,
+              public price: number,
+              public rating: number,
+              public des: string,
+              public categories: Array<string>) {
 
   }
 }
